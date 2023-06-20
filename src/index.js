e6b0ad80751b7d02496d3d2cda35dcdf7a5123f3
Skave-a <arrowtata@gmail.com>
2023-06-20 11:13:36 +0300

    feat: add commands

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { homedir } from 'os';
+import { COMMANDS } from './utils/constants';
 
 const userName = process.argv.length > 2 ? process.argv[2].slice(11) : 'incognito';
 console.log(`Welcome to the File Manager, ${userName}!`);
@@ -8,7 +9,47 @@ console.log(`You are currently in ${curDir}`);
 
 const FM = () => {
   process.stdin.on('data', data => {
+    const dataStringified = data.toString();
+    const command = dataStringified.toString().split(' ')[0];
 
+    switch (command) {
+      case COMMANDS.up:
+        // код для команды up
+        break;
+      case COMMANDS.cd:
+        // код для команды cd
+        break;
+      case COMMANDS.ls:
+        // код для команды ls
+        break;
+      case COMMANDS.add:
+        // код для команды add
+        break;
+      case COMMANDS.cat:
+        // код для команды cat
+        break;
+      case COMMANDS.cp:
+        // код для команды cp
+        break;
+      case COMMANDS.mv:
+        // код для команды mv
+        break;
+      case COMMANDS.rm:
+        // код для команды rm
+        break;
+      case COMMANDS.rn:
+        // код для команды rn
+        break;
+      case COMMANDS.os:
+        // код для команды os
+        break;
+      case COMMANDS.hash:
+        // код для команды hash
+        break;
+      default:
+        // код, который выполнится, если команда неизвестна
+        break;
+    }
   });
   
   process.on('SIGINT', () => {
---
+++ src/utils/constants.js
@@ -0,0 +1,13 @@
+export const COMMANDS = {
+  up: 'up',
+  cd: 'cd',
+  ls: 'ls',
+  add: 'add',
+  cat: 'cat',
+  cp: 'cp',
+  mv: 'mv',
+  rm: 'rm',
+  rn: 'rn',
+  os: 'os',
+  hash: 'hash',
+}
