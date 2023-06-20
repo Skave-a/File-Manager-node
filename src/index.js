0eff262f04cf22655aaee660b6810becefc11612
Skave-a <arrowtata@gmail.com>
2023-06-20 10:52:36 +0300

    feat: add current dir

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
+import { homedir } from 'os';
 
 const userName = process.argv.length > 2 ? process.argv[2].slice(11) : 'incognito';
 console.log(`Welcome to the File Manager, ${userName}!`);
 
+let curDir = homedir();
+console.log(`You are currently in ${curDir}`);
