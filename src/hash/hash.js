076a54a937d04f9c5d2a172972d6bd3bea964525
Skave-a <arrowtata@gmail.com>
2024-02-03 21:16:28 +0300

    fix: hash

--- src/hash/hash.js
+++ src/hash/hash.js
@@ -5,22 +5,27 @@ import { curDir as dir} from '../utils/index.js';
 
 export const hash = (curDir, file) => {
   
-  const pathFile = path.join(curDir, file);
-  const hash = crypto.createHash('sha256');
-  const fileStream = fs.createReadStream(pathFile);
-
-  fileStream.on('data', (data) => {
-    hash.update(data);
-  });
-
-  fileStream.on('end', () => {
-    const fileHash = hash.digest('hex');
-    console.log(`Hash - ${fileHash}`);
-    dir(curDir);
-  });
-
-  fileStream.on('error', (err) => {
-    console.log(err.message);;
-  });
-
+  try {
+    const pathFile = path.join(curDir, file);
+    const hash = crypto.createHash('sha256');
+    const fileStream = fs.createReadStream(pathFile);
+  
+    fileStream.on('data', (data) => {
+      hash.update(data);
+    });
+  
+    fileStream.on('end', () => {
+      const fileHash = hash.digest('hex');
+      console.log(`Hash - ${fileHash}`);
+      dir(curDir);
+    });
+  
+    fileStream.on('error', (err) => {
+      console.log(err.message);
+      console.log('Invalid command');
+    });
+  } catch (err) {
+    console.log(err.message);
+    console.log('Invalid command');
+  }
 };
