619cd801470967e8451b0ccd81a77806b33f39f8
Skave-a <arrowtata@gmail.com>
2024-02-03 20:29:21 +0300

    fix: rn

--- src/fs/rn.js
+++ src/fs/rn.js
@@ -4,10 +4,13 @@ import path from 'path';
 export const rn = async (curDir, file, secondfile) => {
 
   const oldName = path.join(curDir, file);
-  const newName = path.join(curDir, secondfile);
 
-  fs.rename(oldName, newName, (err) => {
-    if (err) throw err;
+  try {
+    const newName = path.join(curDir, secondfile);
+
+    fs.rename(oldName, newName, (err) => {
   });
-
+  } catch (err) {
+    console.log(err.message);
+  }
 };
