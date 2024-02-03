87e00c14becae608f44e2d5e1c40f4f58852c644
Skave-a <arrowtata@gmail.com>
2024-02-03 21:08:07 +0300

    fix: mv

--- src/fs/mv.js
+++ src/fs/mv.js
@@ -5,25 +5,27 @@ export const mv = async (curDir, file, targetDir) => {
   const sourcePath = path.join(curDir, file);
 
   try {
+    
     const newName = path.join(curDir, secondfile);
     await fs.promises.access(newName);
     console.log(`File ${secondfile} already exists`);
     return;
   } catch (error) {
-  try {
-    const targetPath = path.join(targetDir, file);
+    try {
+      await fs.promises.access(sourcePath);
+      const targetPath = path.join(targetDir, file);
 
-    const readStream = fs.createReadStream(sourcePath);
-    const writeStream = fs.createWriteStream(targetPath);
+      const readStream = fs.createReadStream(sourcePath);
+      const writeStream = fs.createWriteStream(targetPath);
 
-    readStream.pipe(writeStream);
+      readStream.pipe(writeStream);
 
-    writeStream.on('finish', async () => {
-      await fs.promises.unlink(sourcePath);
-    });
-  } catch (err) {
-    console.log(err.message);
-    console.log('Invalid command');
+      writeStream.on('finish', async () => {
+        await fs.promises.unlink(sourcePath);
+      });
+    } catch (err) {
+      console.log(err.message);
+      console.log('Invalid command');
+    }
   }
-}
 };
